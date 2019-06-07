1bcf211afd09b242ec323a12a33f5013db8decb6
David Yoon <davidyoon85@gmail.com>
2019-06-07 19:15:31 -0400

    app intialize

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import trxData from '../data/trx.json'
+
+class Main extends Component {
+    render() {
+        return (
+            <h1>hi</h1>
+        )
+    }
+}
+
+export default Main;
